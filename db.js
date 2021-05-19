0075cbf2f6f22b3f5fe74d1946929b6ec6facf4e
Deathsight <57475781+Deathsight@users.noreply.github.com>
2021-05-19 15:39:22 +0300

    Firebase and navigation packages.
    Firebase folder (db) linked with the database.
    
    a navigation package for later development

---
+++ db.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDWos6YkrCVXqqnn0jbfpZVNJNmcyy_SpU",
+    authDomain: "parking-bf553.firebaseapp.com",
+    databaseURL: "https://parking-bf553-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "parking-bf553",
+    storageBucket: "parking-bf553.appspot.com",
+    messagingSenderId: "407394943830",
+    appId: "1:407394943830:web:5e06eff6b8179958c14cf7"
+  };
+
+  firebase.initializeApp(config);
+
+export default firebase.firestore();
