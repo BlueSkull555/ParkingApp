dd54476f1da46263b3f45d7dda93ea617a7e8667
Deathsight <57475781+Deathsight@users.noreply.github.com>
2021-05-27 14:35:12 +0300

    Updated Colors

---
+++ Color.js
@@ -0,0 +1,25 @@
+module.exports = {
+  black: {
+    dark: "#0B090A",
+    light: "#161A1D",
+  },
+
+  gray: {
+    dark: "#403D39",
+    silver: "#B1A7A6",
+    light: "#D3D3D3",
+    verylight: "#F5F3F4",
+  },
+
+  white: "#FFFFFF",
+  red: {
+    dark: "#660708",
+    ruby: "#A4161A",
+    red: "#BA181B",
+    light: "#E5383B",
+  },
+  validation: {
+    error: "#B55B5B",
+    succes: "#85BD6A",
+  },
+};
